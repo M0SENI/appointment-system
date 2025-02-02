7d9645e70f0d7dc8553f7ebacbdee67a5b730253
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-02 17:27:08 +0330

    views compeleted

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 
-# Create your views here.
+
+class LoginView(LoginRequiredMixin, View):
+    login_url = "/users/"
+    redirect_field_name = "redirect_to"
+    def email_check(self):
+        print('email_check')
+        return self.request.user.username.startswith("admin")
+    def password_check(self):
+        print('password_check')
+        return self.request.user.password.startswith("<PASSWORD>")
+    def post(self, request, *args, **kwargs):
+        if not self.email_check():
+            raise PermissionDenied
