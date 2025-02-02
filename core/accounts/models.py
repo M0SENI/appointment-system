35957abe56f29dfb069eaa9b7cc159776caa2d31
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-02 16:48:13 +0330

    CustomUser model has been compeleted

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from django.db.models import *
+from django.contrib.auth.models import BaseUserManager , AbstractBaseUser , PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = EmailField(max_length=255, unique=True)
+    USERNAME_FIELD = 'email'
+    is_staff = BooleanField(default=False)
+    is_superuser = BooleanField(default=False)
+    is_active = BooleanField(default=True)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
