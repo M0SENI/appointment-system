b5f96305a64f05e8e76e61546dbfb2345c1fdfc5
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-02 17:26:49 +0330

    admin configed

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_staff', 'is_superuser', 'is_active')
+    searching_fields = ('email')
+    ordering = ['created_at']
+    fieldsets = (
+        ("Privacy", {"fields": ("email", "password")}),
+        ("Acces", {"fields": ("is_staff", "is_active", "is_superuser")}),
+        ("Permissions", {"fields": ("groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login",)})
+    )
+    add_fieldsets = (
+        ('Authentication',
+         {"fields": ("email", "password1", "password2")}), (
+            "Access",
+            {"fields": ("is_staff", "is_active", "is_superuser")}))
+
+
+admin.site.register(Profile)
+admin.site.register(User, CustomUserAdmin)
