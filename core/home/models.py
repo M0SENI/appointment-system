96ab9d3dfbc2343a3ed5d18f5321617f09c9e1c9
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-02 17:39:59 +0330

    Appontment model added

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from core.accounts.models import Profile
+from django.http import request
 
-# Create your models here.
+
+class Appointment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    request = models.TextField(blank=True)
+    sent_date = models.DateField(auto_now_add=True)
+    accepted = models.BooleanField(default=False)
+    accepted_date = models.DateField(auto_now_add=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ["-sent_date"]
