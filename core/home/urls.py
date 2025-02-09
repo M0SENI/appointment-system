609c8d611dd61f870034565ae48843dbd2eced6d
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-09 19:47:59 +0330

    urls | old urls are destroyed and here we have new urls

--- core/home/urls.py
+++ core/home/urls.py
@@ -3,13 +3,13 @@ from .views import *
 from django.conf.urls.static import static
 
 urlpatterns = [
+
     path("", HomeTemplateView.as_view(), name="home"),
-    path("make-an-appointment/", AppointmentAddView.as_view(), name="appointment"),
-    path("manage-appointments/", ManageAppointmentTemplateView.as_view(), name="manage"),
-    path("manage-appointments/update/", ManageAppointmentTemplateView.as_view(), name="manage"),
-    path("manage-appointments/update/<int:pk>/" , AppointmentUpdateView.as_view(), name="update-appointment" ),
-    # path("manage-appointments/accepted/" , AppointmentUpdateView.as_view(), name="update-appointment" ),
-    # path("manage-appointments/rejected/" , AppointmentUpdateView.as_view(), name="update-appointment" ),
+    path ('add-appointment/' , AppointmentAddView.as_view(), name="add-appointment"),
+    path('manage-appointments/' , ManageAppointmentTemplateView.as_view(), name="manage-appointments" ),
+    path('contact-us/' , ContactUsView.as_view(), name="contact-us" ),
+    path('about/' , AboutView.as_view(), name="about-us" ),
+    path('faq/' , FaqView.as_view(), name="faq" ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
