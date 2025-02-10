df57cbb88702631c4f8d066cdb70fe708d74d588
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-10 15:24:18 +0330

    urls | empty commit

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -3,9 +3,9 @@ from .views import *
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('verify/', VerifyView.as_view(), name='verify'),
     path('verify-code/', VerifyCodeView.as_view(), name='verify-code'),
     path('verify/password/', SetPasswordView.as_view(), name='passwords'),
+    path('logout/', logout_view, name='logout'),
 
 
 
