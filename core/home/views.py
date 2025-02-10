a0b06ec6f1a051b530ddd393e86e15f01495103d
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-10 15:26:08 +0330

    views | HomeTemplateView is updated

--- core/home/views.py
+++ core/home/views.py
@@ -1,35 +1,64 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from django.shortcuts import render , redirect
 from django.views.generic.base import TemplateView
-from django.core.mail import EmailMessage, message
 from django.conf import settings
-from .models import Appointments
-from django.views.generic import ListView, CreateView, UpdateView, FormView
-import datetime
-from django.template import Context
-from django.template.loader import render_to_string, get_template
-from .forms import *
-from django.contrib.auth.mixins import LoginRequiredMixin , PermissionRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, FormView , View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages import *
+import random
+import string
+from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from .forms import *
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class HomeTemplateView(CreateView , LoginRequiredMixin):
-    model = Appointments
-    form_class = AppointmentForm
+def generate_code():
+    return str(random.randint(100000, 999999))
+
+
+class HomeTemplateView(View):
     template_name = 'index.html'
-    success_url = '/make-an-appointment/'
 
-    def form_valid(self ,  form):
-        if not form.is_valid():
-            error(self.request ,"فرم شما ثبت نشد , لطفا مجددا امتحان کنید.")
-            return self.form_invalid(form)
-        form.instance.user = self.request.user
-        form.instance.save()
-        success(self.request , "فرم شما ثبت و ارسال شد!")
-        return super().form_valid(form)
+    def post(self, request, *args, **kwargs):
+        first_form = AppointmentForm(request.POST)
+        second_form = VerifyForm(request.POST)
 
+        if 'submit_first_form' in request.POST:
+            if first_form.is_valid():
+                first_form.instance.user = self.request.user
+                first_form.instance.save()
+                success(self.request, "فرم شما ثبت و ارسال شد!")
+                return redirect('home')
+            else:
+                error(self.request, "فرم شما ثبت نشد، لطفا مجددا امتحان کنید.")
 
+        if 'submit_second_form' in request.POST:
+            if second_form.is_valid():
+                email = second_form.cleaned_data['email']
+                user = User.objects.filter(email=email).first()
+                code = generate_code()
+                self.request.session['email'] = email
+                self.request.session['code'] = code
+                if user:
+                    send_mail('Login Code', f'Your Login code is: {code}', from_email=settings.EMAIL_HOST_USER,
+                              recipient_list=[email])
+                else:
+                    send_mail('SignUp Code', f'Your sign up code is: {code}', from_email=settings.EMAIL_HOST_USER,
+                              recipient_list=[email])
+                success(self.request, "کد به ایمیل شما ارسال شد!")
+                return redirect('verify-code')
+            else:
+                error(self.request, "ایمیل معتبر نمی‌باشد.")
 
+        return render(request, self.template_name, {'first_form': first_form, 'second_form': second_form})
+
+    def get(self, request, *args, **kwargs):
+        first_form = AppointmentForm()
+        second_form = VerifyForm()
+        return render(request, self.template_name, {'first_form': first_form, 'second_form': second_form})
 class ManageAppointmentTemplateView(ListView):
     template_name = "manage-appointments.html"
     model = Appointments
@@ -65,6 +94,15 @@ class AppointmentUpdateView(UpdateView):
         response = super().form_valid(form)
         success(self.request , "Appointment updated successfully!")
         return response
+
+
+class VerifyView(FormView):
+    model = User
+    template_name = 'index.html'
+    form_class = VerifyForm
+    success_url = reverse_lazy('verify-code')
+
+
 
 
 
