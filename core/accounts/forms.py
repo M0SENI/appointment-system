1fe4c8d953eac9209ffc583534899b6f4cd4c6fe
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-10 15:23:47 +0330

    forms | empty commit

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -2,15 +2,12 @@ from django.contrib.auth import get_user_model
 from django import forms
 
 
-class VerifyForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control' , 'name' : 'dzEmail' , 'placeholder' :  'ایمیل' , 'required' : True , 'type' : 'email'}))
-
 class VerifyCodeForm(forms.Form):
     code = forms.CharField(max_length=6, widget=forms.Textarea(attrs={'class' : 'form-control' , 'required' : True , 'placeholder' : "کد شش رقمی"}))
 
 class SetPasswordForm(forms.Form):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control border-rounded'}), label='پسورد')
-    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control border-rounded'}), label='تایید پسورد')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
 
     def clean(self):
         cleaned_data = super().clean()
