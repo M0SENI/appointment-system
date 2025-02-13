9362863ed3a680aa1e63c641d6aa4d291a165561
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-13 13:05:03 +0330

    contact form has been added

--- core/home/forms.py
+++ core/home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Appointments
+from .models import Appointments , Contact
 
 class AppointmentForm(forms.ModelForm):
     email = forms.EmailField(widget=forms.HiddenInput(), required=False)
@@ -38,3 +38,24 @@ class UpdateAppointmentForm(forms.ModelForm):
 
 class VerifyForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control' , 'placeholder' :  'ایمیل' , 'required' : True , 'type' : 'email'}))
+
+
+class ContactForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Contact
+        fields = ['first_name', 'last_name', 'phone', 'request', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام (به فارسی)' , 'id' : 'inputYourName' ,'require' : True}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام خانوادگی(به فارسی)' , 'id' : 'inputYourLastName' , 'require' : True}),
+            'phone': forms.TextInput(attrs={'class': 'form-control dz-number', 'placeholder': 'شماره تماس' , 'id' : 'inputYourPhone' , 'name':'dzPhoneNumber' , 'require' : True}),
+            'request': forms.Textarea(attrs={'class': 'form-control', 'placeholder':  'یادداشت' , 'id': 'inputYourRequest' , 'require' : True}),
+        }
+
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(ContactForm, self).__init__(*args, **kwargs)
+        if user:
+            self.fields['email'].initial = user.email
