80435b7ec63f2ace94a500941ddfd142701e0d8b
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-02-19 20:46:57 +0330

    RU for staff's and RUD for users added for control appointments

--- core/home/urls.py
+++ core/home/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
 
     path("", HomeTemplateView.as_view(), name="home"),
     path ('add-appointment/' , AppointmentAddView.as_view(), name="add-appointment"),
-    path('manage-appointments/' , ManageAppointmentTemplateView.as_view(), name="manage-appointments" ),
+    path('appointment-list/' , AppointmentListView.as_view(), name="appointments-list" ),
+    path('appointment-list/edit/<int:pk>' , AppointmentUpdateView.as_view(), name="edit-appointments" ),
+    path('manage-appointments/' , ManageAppointments.as_view(), name="manage-appointments" ),
+    path('manage-appointments/<int:pk>' , EditAppointmentsView.as_view(), name="user-edit-appointments" ),
+    path('delete-appointments/<int:pk>' , DeleteAppointmentView.as_view(), name="user-delete-appointments" ),
     path('contact-us/' , ContactUsView.as_view(), name="contact-us" ),
     path('about/' , AboutView.as_view(), name="about-us" ),
     path('faq/' , FaqView.as_view(), name="faq" ),
