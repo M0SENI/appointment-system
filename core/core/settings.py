2457f9198aed6a9e71b98e32a5a4eb62f8a74777
Mohsen qorbani <157234876+M0SENI@users.noreply.github.com>
2025-03-10 13:43:01 +0330

    settings | statig bugs fixed

--- core/core/settings.py
+++ core/core/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_jv9em#$0ujfxnf)0wy1ebg-a-h6bjkh^che+b@oo8v_swcqsp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
